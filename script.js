03a0cde376e82fb44a0ad0aef5bb3fb5f97d43b8
Priyanshi Kathayat <priyanshikathayat30@gmail.com>
2025-05-01 20:45:12 +0530

    Priyanshi Contribution

---
+++ script.js
@@ -0,0 +1,11 @@
+// Scroll to Top Function (Navigarion)
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+  
+  // Scroll to Footer Function
+  function scrollToFooter() {
+    const footer = document.getElementById('footer');
+    footer.scrollIntoView({ behavior: 'smooth' });
+  }
+  
